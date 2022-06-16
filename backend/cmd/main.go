ba98932a58bdacac5204577ca42a295ac460d5d7
TIshow <febooooooo@gmail.com>
2022-06-16 21:50:57 +0900

    Route GraphQL with Gin

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -9,18 +9,58 @@ import (
 	"github.com/TIshow/learn-to-donate/cmd/loader"
 	"github.com/TIshow/learn-to-donate/graph"
 	"github.com/TIshow/learn-to-donate/graph/generated"
+	"github.com/gin-gonic/gin"
 )
 
 const defaultPort = "3306"
 
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
 func main() {
 	loader.EnvLoad() // Load from .env file
+	router := gin.Default()
+	router.Use(CORSMiddleware())
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router.GET("/", playgroundHandler())
+	router.POST("/query", graphqlHandler())
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Run(":3306")
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", "3306")
 	log.Fatal(http.ListenAndServe(":"+"3306", nil))
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
